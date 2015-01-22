39cebcda606144af998fa06efe3d8dcd4d5855d2
Tan Tian Xiang <tantianx@gmail.com>
2015-01-21 22:15:13 -0800

    Initial Commit

---
+++ CSCI580_RenderEngine/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CSCI580_RenderEngine
+//
+//  Created by Tan Tian Xiang on 1/21/15.
+//  Copyright (c) 2015 tantianx. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
