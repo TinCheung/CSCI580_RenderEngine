3bca51d4842045de96aaabd806d9c6c05385909a
Tan Tian Xiang <tantianx@gmail.com>
2015-03-29 17:46:59 -0700

    Finish hw6

---
+++ Application6.cpp
@@ -0,0 +1,9 @@
+//
+//  Application6.cpp
+//  CSCI580_RenderEngine
+//
+//  Created by Tan Tian Xiang on 15/3/26.
+//  Copyright (c) 2015年 tantianx. All rights reserved.
+//
+
+#include <stdio.h>
---
+++ Application6.h
@@ -0,0 +1,13 @@
+//
+//  Application6.h
+//  CSCI580_RenderEngine
+//
+//  Created by Tan Tian Xiang on 15/3/26.
+//  Copyright (c) 2015年 tantianx. All rights reserved.
+//
+
+#ifndef CSCI580_RenderEngine_Application6_h
+#define CSCI580_RenderEngine_Application6_h
+
+
+#endif
